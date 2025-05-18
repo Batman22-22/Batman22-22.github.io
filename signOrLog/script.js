a1beefb9e7a3cd26d1618ca813266e9f6922037c
Batman22-22 <160787227+Batman22-22@users.noreply.github.com>
2025-05-18 22:36:14 +0000

    Changed purpose of code

--- signOrLog/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("button1").onclick = function () {
-    window.location.href = "../../Capstone/register";
-};
-
-document.getElementById("button2").onclick = function () {
-    window.location.href = "../../Capstone/Login";
-};
