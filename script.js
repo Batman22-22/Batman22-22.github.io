7746b725755902106cd1831d7f367289aff6c0f9
Batman22-22 <160787227+Batman22-22@users.noreply.github.com>
2025-05-19 01:29:23 +0000

    added script and edited css

---
+++ script.js
@@ -0,0 +1,39 @@
+// buttons  for Calender Quest
+document.getElementById("calenderbutton1").onclick = function () {
+    window.open("https://batman22-22.github.io/Calender-Quest/Capstone/welcome/");
+};
+
+document.getElementById("calenderbutton2").onclick = function () {
+    window.open("https://github.com/Batman22-22/Calender-Quest.git");
+};
+
+$("#hideCalender").click(function(){
+    $("#calender").hide();
+    $("#showCalender").css("display", "");
+  });
+  
+  $("#showCalender").click(function(){
+    $("#calender").show();
+    $("#showCalender").css("display", "none");
+  });
+
+
+
+  // buttons for Candle Climber
+document.getElementById("candlebutton1").onclick = function () {
+    window.open("https://blank.page/");
+};
+
+document.getElementById("candlebutton2").onclick = function () {
+    window.open("https://blank.page/");
+};
+
+$("#hideCandle").click(function(){
+    $("#candle").hide();
+    $("#showCandle").css("display", "");
+  });
+  
+  $("#showCandle").click(function(){
+    $("#candle").show();
+    $("#showCandle").css("display", "none");
+  });
